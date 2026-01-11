48f3fd9ab9510c73762ab9ae000ea1e43258d47d
Priyansukumar001 <priyanshukumar78760@gmail.com>
2026-01-11 12:25:46 +0530

    Add files via upload

---
+++ poem.py
@@ -0,0 +1,11 @@
+print("POEM:-")
+print("")
+print('''✨ Twinkle, Twinkle, Little Star ✨
+
+Twinkle, twinkle, little star,
+How I wonder what you are!
+Up above the world so high,
+Like a diamond in the sky.
+
+Twinkle, twinkle, little star,
+How I wonder what you are! 🌟''')
